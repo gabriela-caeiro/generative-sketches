6bdd9fee6be03165a87422d49fd87af3152ff15b
Gabriela Caeiro <gabriela.caeiro.topa@gmail.com>
2023-01-14 03:54:33 +0100

    Lines added connecting the dots´+ video output with ffmpeg

--- sketch-03.js
+++ sketch-03.js
@@ -1,5 +1,6 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
   dimensions: [ 1080, 1080 ],
@@ -16,15 +17,35 @@ animate(); */
 const sketch = ({ context, width, height }) => {
   const agents= [];
 
-  for (let i = 0; i < 60; i++){
+  for (let i = 0; i < 55; i++){
     const x = random.range(0, width);
     const y = random.range(0, height);
     agents.push(new Agent(x, y));
   }
 
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < agents.length; i++) {
+      const agent = agents[i];
+
+      for (let j = i + 1; j < agents.length; j++) {
+        const other = agents[j];
+
+        const dist = agent.pos.getDistance(other.pos); 
+
+        if (dist > 200) continue;
+
+        context.lineWidth = math.mapRange(dist, 0, 200, 7, 0.7);
+
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.strokeStyle = "white";
+        context.stroke();
+      }
+    }
 
     agents.forEach(agent => {
       agent.update();
@@ -57,15 +78,20 @@ class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-   
+  }
+
+  getDistance(v) {
+    const dx = this.x - v.x;
+    const dy = this.y - v.y;
+    return Math.sqrt(dx * dx + dy * dy);
   }
 }
 
 class Agent {
   constructor(x, y) {
     this.pos = new Vector(x, y);
-    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
-    this.radius = random.range(4, 12);
+    this.vel = new Vector(random.range(-1.2, 1.2), random.range(-1.2, 1.2));
+    this.radius = random.range(4, 11);
   }
 
   bounce(width, height) {
@@ -84,12 +110,12 @@ class Agent {
     context.save();
     context.translate(this.pos.x, this.pos.y);
 
-    //context.lineWidth = 4;
+    context.lineWidth = 2;
   
     context.beginPath();
     context.arc(0, 0, this.radius, 0, Math.PI * 2);
     context.fill();
-    //context.stroke();
+    context.stroke();
 
     context.restore();
   }
